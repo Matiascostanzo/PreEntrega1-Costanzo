5704e5d9efec6a841d2b3596ad1ad1038bd3b643
Matiascostanzo <matiascostanzo@live.com.mx>
2023-03-25 13:25:58 -0300

    Agrego función para poner en mayuscula la primer letra del país, entonces si el usuario elige un pais pero lo escribe todo en minuscula o todo en mayuscula, la función pone la primer letra en mayuscula y el resto en minuscula como están dados de alta los paises en el array

---
+++ variables.js
@@ -0,0 +1,45 @@
+const africa = [
+  "Egipto",
+  "Marruecos",
+  "Nigeria",
+  "Sudáfrica",
+  "Túnez",
+  "Argelia",
+];
+const america = [
+  "Argentina",
+  "Brasil",
+  "Canadá",
+  "Estados Unidos",
+  "México",
+  "Colombia",
+  "Ecuador",
+  "Costa Rica",
+];
+const asia = [
+  "China",
+  "India",
+  "Japón",
+  "Rusia",
+  "Turquía",
+  "Korea del Sur",
+  "Indonesia",
+  "Tailandia",
+];
+const europa = [
+  "Alemania",
+  "España",
+  "Francia",
+  "Italia",
+  "Reino Unido",
+  "Islandia",
+  "Noruega",
+  "Dinamarca",
+];
+const oceania = [
+  "Australia",
+  "Fiyi",
+  "Nueva Zelanda",
+  "Papúa Nueva Guinea",
+  "Tonga",
+];
