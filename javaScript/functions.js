43d25dacdab69fef5ee4b95203c7de34cad68d1e
Matias Costanzo <matiascostanzo@live.com.mx>
2023-04-10 10:15:43 -0300

    agrego los comentarios

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -5,6 +5,7 @@
 // Paso 5: Preguntarle si quiere seguir agregando algun destino, si si, volver a solicitar que el usuario ingrese un codigo, sino, mostrar el total del carrito y finalizar
 let destinoEncontrado = [];
 
+//Recorre el array de destinos con un forEach y los muestra
 function mostrarDestinos() {
   console.log("Destinos disponibles:");
   destinos.forEach(function (destino) {
@@ -12,11 +13,13 @@ function mostrarDestinos() {
   });
 }
 
+//Recorre el array de carrito de compras con un forEach y los muestra
 function mostrarCarrito() {
   console.log("Estas son tus compras:");
   carrito.forEach(function (destino) {
     console.log(destino);
   });
+  //Instancio un objeto con el constructor de la clase Eleccion para mostrar el total del carrito
   const precioTotal = new Eleccion(carrito);
   console.log(
     "El precio total de las excursiones en el carrito es de: " +
@@ -24,10 +27,12 @@ function mostrarCarrito() {
   );
 }
 
+//El usuario elije el codigo del destino que quiere visitar
 function elegirDestino() {
   let destinoElegido = parseInt(
     prompt("Elegir el codigo del destino que quieres visitar")
   );
+  //Busca el codigo ingresado por el usuario en el array destinos
   destinoEncontrado = destinos.find(
     (destino) => destino.codigo === destinoElegido
   );
@@ -52,7 +57,7 @@ function agregarAlCarrito() {
   let eleccion = confirm(
     "¿Quieres agregar ese destino a tu carrito de compras?"
   );
-
+  //Si el usuario confirma que quiere agregar esa excursion, la agrega al carrito
   if (eleccion === true) {
     console.log("Se agregó la excursion al carrito");
     carrito.push(destinoEncontrado);
@@ -61,6 +66,7 @@ function agregarAlCarrito() {
   }
 }
 
+//Si el usuario quiere seguir agregando excursiones, llamo a las funciones elegisDestino y agregarAlCarrito
 function elegirOtro() {
   let decision = true;
   while (decision) {
@@ -72,6 +78,7 @@ function elegirOtro() {
   }
 }
 
+//Funcion principal que llama al resto de las funciones y al final reinicio el carrito a 0
 function compras() {
   mostrarDestinos();
   elegirDestino();
